b402d8ce4f4845f40ddc2b07e7c8214f21149646
Lupu Alexandru Stefan <alex.loopu@gmail.com>
2021-10-15 22:33:48 +0300

    Add markers for page / layout sections

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <!-- TODO: Implement the header component -->
+    <!-- <TheHeader /> -->
+    <Nuxt />
+    <!-- TODO: Implement the footer component -->
+    <!-- <TheFooter /> -->
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,22 @@
 <template>
-  <Tutorial />
+  <div>
+    <section id="top-section">
+      <!-- TODO: Create the top section -->
+      <!-- @Mihai Iov -->
+    </section>
+
+    <section id="about">
+      <!-- TODO: Create the about section -->
+    </section>
+
+    <section id="testimonials">
+      <!-- TODO: Create the testimonials section -->
+    </section>
+
+    <section id="register">
+      <!-- TODO: Create the registration section -->
+    </section>
+  </div>
 </template>
 
 <script>
