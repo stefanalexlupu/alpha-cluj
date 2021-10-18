5d9fd323ca253baaa38624651a5b7221c40162e4
Vitalie <vitalie.cirjeleanu@gmail.com>
2021-10-18 22:02:51 +0300

    TestimonialCard component added

---
+++ components/TestimonialCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class=" p-8 bg-foregreground-base rounded ">
+    <div class="flex items-center gap-4 mb-4">
+      <img
+        :src="image"
+        alt="image"
+        class=" w-16 h-16 object-cover rounded-full"
+      />
+      <span>
+        <p class=" text-2xl text-white ">{{ name }}</p>
+      </span>
+    </div>
+    <p class=" text-base text-white">
+      {{ text }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestimonialCard',
+  props: {
+    image: {
+      type: String,
+      default: ''
+    },
+    name: {
+      type: String,
+      default: ''
+    },
+    text: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      foregreground: {
+        base: '#1E2433',
+        dark: '#101217'
+      },
+      white: '#fff'
+    },
     extend: {}
   },
   variants: {
