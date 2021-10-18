5638d3941a234fe5160339e6b7aba7146da57753
Lupu Alexandru Stefan <alex.loopu@gmail.com>
2021-10-18 18:19:58 +0300

    add foreground color to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        foreground: {
+          base: '#1E2433'
+        }
+      }
+    }
   },
   variants: {
     extend: {}
