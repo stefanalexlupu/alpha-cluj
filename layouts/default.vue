0e48d2324fa261608b6a7ac8315e70065692cc9f
Mihai Razvan <me@mihai.com>
2021-10-20 16:55:12 +0300

    add the navbar template to the default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,7 @@
   <div>
     <!-- TODO: Implement the header component -->
     <!-- <TheHeader /> -->
+    <TheNavBar />
     <Nuxt />
     <!-- TODO: Implement the footer component -->
     <!-- <TheFooter /> -->
