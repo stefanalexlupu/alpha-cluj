9e35362dee3d97752de4e60522b2d3d9ffe2bb2c
Mihai Razvan <me@mihai.com>
2021-10-20 11:01:11 +0300

    refactor header style

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,13 +1,15 @@
 <template>
-  <header class="w-full relative bg-center bg-no-repeat bg-cover bg-gray-500">
-    <div class="hero-content container mx-auto px-4 relative h-full z-20">
-      <div class="hero-text-left relative">
+  <header class="w-full h-full relative bg-center bg-no-repeat bg-cover bg-gray-500 md:h-screen before:absolute">
+    <div class="container mx-auto px-4 relative h-full z-20 flex flex-col md:grid md:grid-cols-2 md:justify-center md:items-center">
+      <div class="relative mt-36 mb-12 text-center md:text-left md:pr-4 md:m-0">
         <h1 class="text-4xl text-white">Lorem ipsum</h1>
         <p class="text-base text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aperiam, at distinctio doloremque doloribus esse facere impedit inventore laborum modi mollitia natus nesciunt nihil nostrum odit porro quibusdam quidem reiciendis repellendus soluta sunt ut voluptatem.</p>
         <p class="text-base text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque dolorem iusto, nisi quas quia tempore unde? Assumenda dolores expedita officiis rerum! Adipisci architecto atque debitis earum eos esse in nam?</p>
-        <Button @click="goToLink()" text="Înregistrează-te acum!" />
+        <AlphaButton @click="goToLink()">
+          Înregistrează-te acum!
+        </AlphaButton>
       </div>
-      <div class="hero-video-right relative h-0">
+      <div class="hero-video-right relative h-0 mb-12 md:mb-0">
         <iframe width="100%" height="100%" class="absolute top-0 left-0" src="https://www.youtube.com/embed/GEgSBuYlSoA" />
       </div>
     </div>
@@ -27,7 +29,6 @@ export default {
 
   header {
     background: url("static/bg.jpg");
-    height: 100%;
   }
 
   header::before {
@@ -38,41 +39,7 @@ export default {
     background: linear-gradient(180deg, rgba(196, 196, 196, 0) 0%, rgba(9, 9, 9, 0.42) 38.43%);
   }
 
-  .hero-content {
-    display: flex;
-    flex-direction: column;
-  }
-
-  .hero-text-left {
-    margin: 130px 0 50px;
-    text-align: center;
-  }
-
   .hero-video-right {
     padding-bottom: 56%;
-    margin-bottom: 50px;
-  }
-
-  @media (min-width: 768px) {
-    header {
-      height: 100vh;
-    }
-
-    .hero-content {
-      display: grid;
-      grid-template-columns: 1fr 1fr;
-      justify-content: center;
-      align-items: center;
-    }
-
-    .hero-text-left {
-      margin: unset;
-      padding-right: 1rem;
-      text-align: left;
-    }
-
-    .hero-video-right {
-      margin-bottom: unset;
-    }
   }
 </style>
