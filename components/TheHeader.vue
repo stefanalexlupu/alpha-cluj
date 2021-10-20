be900f7ba21b93969660cb50f12e4cdb06541141
Mihai Razvan <me@mihai.com>
2021-10-20 16:48:12 +0300

    remove z-20 style on

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,13 +1,11 @@
 <template>
   <header class="w-full h-full relative bg-center bg-no-repeat bg-cover bg-gray-500 md:h-screen before:absolute">
-    <div class="container mx-auto px-4 relative h-full z-20 flex flex-col md:grid md:grid-cols-2 md:justify-center md:items-center">
+    <div class="container mx-auto px-4 relative h-full flex flex-col md:grid md:grid-cols-2 md:justify-center md:items-center">
       <div class="relative mt-36 mb-12 text-center md:text-left md:pr-4 md:m-0">
         <h1 class="text-4xl text-white">Lorem ipsum</h1>
         <p class="text-base text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aperiam, at distinctio doloremque doloribus esse facere impedit inventore laborum modi mollitia natus nesciunt nihil nostrum odit porro quibusdam quidem reiciendis repellendus soluta sunt ut voluptatem.</p>
         <p class="text-base text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque dolorem iusto, nisi quas quia tempore unde? Assumenda dolores expedita officiis rerum! Adipisci architecto atque debitis earum eos esse in nam?</p>
-        <AlphaButton @click="goToLink()">
-          Înregistrează-te acum!
-        </AlphaButton>
+        <Button @click="goToLink()" text="Înregistrează-te acum!" />
       </div>
       <div class="hero-video-right relative h-0 mb-12 md:mb-0">
         <iframe width="100%" height="100%" class="absolute top-0 left-0" src="https://www.youtube.com/embed/GEgSBuYlSoA" />
