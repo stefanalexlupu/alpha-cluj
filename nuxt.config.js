43ac583ff0778160d1a5930983466d3ddff311da
Lupu Alexandru Stefan <alex.loopu@gmail.com>
2021-10-23 09:53:04 +0300

    update page meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,9 +4,9 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'alpha-cluj',
+    title: 'Try Alpha',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'ro'
     },
     meta: [
       { charset: 'utf-8' },
