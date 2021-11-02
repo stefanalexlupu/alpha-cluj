1a1e38b94edb0af256e3c258b46caef00b9be7c3
Lupu Alexandru Stefan <alex.loopu@gmail.com>
2021-11-02 15:26:40 +0200

    fix instagram link

--- components/TheSocialMediaSidebar.vue
+++ components/TheSocialMediaSidebar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="fixed right-0 top-3/4 text-xl">
-    <a href="https://www.instagram.com/try.alpha.betel/" target="_blank" rel="noopener noreferrer" class="flex items-center justify-center w-8 h-8 text-white cursor-pointer mb-2" style="background: radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%, #d6249f 60%, #285aeb 90%);">
+    <a href="https://www.instagram.com/tryalpha.betel/" target="_blank" rel="noopener noreferrer" class="flex items-center justify-center w-8 h-8 text-white cursor-pointer mb-2" style="background: radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%, #d6249f 60%, #285aeb 90%);">
       <FontAwesomeIcon :icon="['fab', 'instagram']" />
     </a>
     <a href="https://www.facebook.com/TryAlpha-105374005274750" target="_blank" rel="noopener noreferrer" class="flex items-center justify-center w-8 h-8 text-white" style="background-color: #4267b2">
